aca30a1600a192b8ad5e9da2f297f8e85a3b8f06
nicholaslaid <129122108+nicholaslaid@users.noreply.github.com>
2023-09-30 01:16:54 -0300

    Update Script.sql

--- Scripts/Script.sql
+++ Scripts/Script.sql
@@ -1,3 +1,4 @@
+SELECT * FROM logs;
 DROP TABLE IF EXISTS logs;
 CREATE TABLE IF NOT EXISTS logs(
 	id BIGSERIAL PRIMARY KEY,
@@ -5,5 +6,14 @@ CREATE TABLE IF NOT EXISTS logs(
 	type VARCHAR(10),
 	message TEXT
 );
-
-SELECT * FROM logs;
+SELECT * FROM produtos;
+DROP TABLE IF EXISTS produtos;
+CREATE TABLE IF NOT EXISTS produtos(
+id serial PRIMARY KEY,
+nome VARCHAR(50),
+marca VARCHAR(50),
+volume real,
+tipo VARCHAR(30),
+data time,
+frasco VARCHAR(30)
+);
